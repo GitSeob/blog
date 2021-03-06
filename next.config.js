d1f56b8c878c934602c07b977239d98a0ed687d4
GitSeob <anhs0220@gmail.com>
2021-03-06 17:59:39 +0900

    next.config.js 에서 request header 부분 함수 제거

--- next.config.js
+++ next.config.js
@@ -12,22 +12,4 @@ module.exports = {
 		SERVER_URL: process.env.NODE_ENV === 'production' ? process.env.PROD_SERVER_URL : process.env.DEV_SERVER_URL,
 		DISQUS_URL: process.env.PROD_DISQUS_URL,
 	},
-	async headers() {
-		return [
-			{
-				// matching all API routes
-				source: '/api/:path*',
-				headers: [
-					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
-					{ key: 'Access-Control-Allow-Origin', value: '*' },
-					{ key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-					{
-						key: 'Access-Control-Allow-Headers',
-						value:
-							'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-					},
-				],
-			},
-		];
-	},
 };
