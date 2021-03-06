17d55b4c8d9af180111fd390df8e1901a58e8a04
GitSeob <anhs0220@gmail.com>
2021-03-06 20:07:03 +0900

    _app.tsx 에서 og:image 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,10 +16,6 @@ const App = ({ Component, pageProps }: AppProps) => {
 				<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 				<meta name="description" content="좋은 개발자를 꿈꾸는 주니어 개발자 홍섭의 블로그입니다." />
 				<meta property="og:type" content="blog" />
-				<meta
-					property="og:image"
-					content="https://gitseob-blog-bucket.s3.ap-northeast-2.amazonaws.com/images/ogImage.png"
-				/>
 				<script async src="https://www.googletagmanager.com/gtag/js?id=G-V9KPRJCZ8R"></script>
 				<script
 					dangerouslySetInnerHTML={{
